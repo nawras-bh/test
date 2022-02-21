c1985ad1b53f59ad8e441c03b554b2101e3708b0
nawras-bh <68807363+nawras-bh@users.noreply.github.com>
2022-02-21 19:42:13 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,9 @@
 output "resource_group_name" {
-  value = azurerm_resource_group.iotstorage.name
+  value = module.storage.resource_group_name
 }
 output "storage_account_name" {
-  value = azurerm_storage_account.edgepfe.name
+  value = module.storage.storage_account_name
 }
-
 output "storage_container_name" {
-  value = azurerm_storage_container.edgeblob.name
+  value = module.storage.storage_container_name
 }
